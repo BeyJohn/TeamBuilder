6c0e1e99552c49ff61dd6dcf193385f10ea8f807
John <Beyjohn1@gmail.com>
2017-04-25 22:08:16 -0500

    Added most functionality TODO CreateTeams DisplayTeams

--- TeamBuilder/MainWindow.xaml.cs
+++ TeamBuilder/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Collections;
+using System;
+using System.IO;
 
 namespace TeamBuilder
 {
@@ -13,7 +15,7 @@ namespace TeamBuilder
 		public MainWindow()
 		{
 			InitializeComponent();
-			
+
 			other = new DisplayTeams(this);
 			teams = new ArrayList();
 		}
@@ -31,7 +33,7 @@ namespace TeamBuilder
 
 		private void AddStudent(object sender, RoutedEventArgs e)
 		{
-			if(tbNewStudent.Text != "" && !cbStudents.Items.Contains(tbNewStudent.Text))
+			if (tbNewStudent.Text != "" && !cbStudents.Items.Contains(tbNewStudent.Text))
 			{
 				cbStudents.Items.Add(tbNewStudent.Text);
 			}
@@ -40,6 +42,55 @@ namespace TeamBuilder
 		private void RemoveStudent(object sender, RoutedEventArgs e)
 		{
 			cbStudents.Items.Remove(cbStudents.Text);
+		}
+
+		private void LoadNames(object sender, RoutedEventArgs e)
+		{
+			if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + tbFileName.Text + ".dat"))
+			{
+				BinaryReader br = new BinaryReader(new FileStream(AppDomain.CurrentDomain.BaseDirectory + tbFileName.Text + ".dat", FileMode.Open));
+				cbStudents.Items.Clear();
+
+				try
+				{
+					string val = br.ReadString();
+					while(val != null)
+					{
+						cbStudents.Items.Add(val);
+						val = br.ReadString();
+					}
+				}
+				catch(Exception) {}
+				br.Close();
+			}
+			else
+			{
+				tbFileName.Text = "FileNotFound";
+			}
+		}
+
+		private void SaveNames(object sender, RoutedEventArgs e)
+		{
+			BinaryWriter bw = new BinaryWriter(new FileStream(AppDomain.CurrentDomain.BaseDirectory + tbFileName.Text + ".dat", FileMode.Create));
+
+			foreach (string h in cbStudents.Items)
+			{
+				bw.Write(h);
+			}
+
+			bw.Close();
+		}
+
+		private void CreateTeams(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+
+			Application.Current.Shutdown();
 		}
 
 	}
